d2314e8c8f94b437185cf51e390882c8d1e0be33
thisisdana49 <thisisdana49@gmail.com>
2025-04-25 14:41:52 +0900

    Initial Commit

---
+++ AppDock/AppDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDockApp.swift
+//  AppDock
+//
+//  Created by 조다은 on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AppDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
